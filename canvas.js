8dcf1ced1b51bc8b6f96ff5a9d32779525c2c195
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2019-03-17 00:49:15 +0100

    Basics working

--- canvas.js
+++ canvas.js
@@ -3,6 +3,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d');
+
+const scheme = new ColorScheme;
+  scheme.from_hue(Math.floor(Math.random() * 361))
+    .scheme('analogic')   
+    // .variation('soft');
+const colors = scheme.colors();
+
 // helpers
 c.moveToPoint = (point) => {
     c.moveTo(point.x, point.y);
@@ -10,7 +17,7 @@ c.moveToPoint = (point) => {
 c.lineToPoint = (point) => {
     c.lineTo(point.x, point.y);
 };
-const createRandomPoint = () => {
+const createRandomPoint = () => { // TODO: don't use random points, use regular ones with variation instead
     let x = Math.random() * (canvas.width+1);
     let y = Math.random() * (canvas.height+1);
     return {x, y};
@@ -21,15 +28,49 @@ const drawPolygon = (points) => {
     points.slice(1, points.length).forEach(point => {
         c.lineToPoint(point);
     })
-    c.lineToPoint(points[0]);
-    c.fillStyle = randomColor();
+    c.fillStyle = `#${colors[Math.floor(Math.random()*colors.length)]}`;
     c.fill();
 };
+
 const createRandomTriangle = () => {
     const points = new Array(3).fill(0).map(() => { return createRandomPoint(); }); // FIXME: Possible to get duplicate points
     drawPolygon(points);
 };
 
-for(let i = 0; i < 10; i++) {
-    createRandomTriangle()
+const createVarriedPointCloud = (amountOfRows, amountOfColumns) => {
+    const pointCloud = new Array(amountOfRows+1).fill(0).map(() => { return new Array(amountOfColumns+1).fill(0); });
+    const widthInterval = canvas.width / amountOfColumns;
+    const heightInterval = canvas.height / amountOfRows;
+    for(let rowIdx = 0; rowIdx <= amountOfRows; rowIdx++) {
+        for(let colIdx = 0; colIdx <= amountOfColumns; colIdx++) {
+            let xVar = (Math.random() * widthInterval) - widthInterval/2;
+            let yVar = (Math.random() * heightInterval) - heightInterval/2;
+            pointCloud[rowIdx][colIdx] = {x: (widthInterval * colIdx) + xVar, y: (heightInterval * rowIdx) + yVar};
+        }
+    }
+    return pointCloud;
 }
+
+let irregularCloud = createVarriedPointCloud(15, 20)
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function draw(revesed) {
+    for(let rowIdx = 1; rowIdx <= irregularCloud.length-1; rowIdx++) {
+        for(let colIdx = 1; colIdx < irregularCloud[rowIdx-1].length; colIdx++) {
+            let col1 = irregularCloud[rowIdx-1][colIdx-1];
+            let col2 = irregularCloud[rowIdx-1][colIdx];
+            let outsider = !revesed ? irregularCloud[rowIdx][colIdx-1] : irregularCloud[rowIdx][colIdx];
+            drawPolygon([col1, col2, outsider])
+            // await sleep(20)
+        }
+    }
+}
+
+draw(false).then(() => {
+    irregularCloud.reverse()
+    draw(true)
+})
+
